13081e6bb265acae7f98f9a8b9a34efed67316dc
lennoxtr <huykhang.tranp@gmail.com>
2023-09-13 11:27:24 +0200

    Changed init messages

--- CameraModules.py
+++ CameraModules.py
@@ -9,26 +9,36 @@ class CameraModule:
     camera_object_list = []
 
     def addCamera(self, camera_address):
-        self.camera_address_list.append(camera_address)
+        self.camera_address_list.append(camera_address)        
+        print('ADD CAMERA ' + camera_address + ' OK')
+
 
     def initializeAllCameras(self):
         for camera_address in self.camera_address_list:
-            print('Found camera: ' + camera_address)
+            print('FOUND CAMERA AT ' + camera_address)
             cap = cv2.VideoCapture(camera_address)
             cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.CAMERA_FRAME_WIDTH)
             cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.CAMERA_FRAME_HEIGHT)
-            self.camera_object_list.append(cap)  
+            self.camera_object_list.append(cap)
+
+        print('INITIALIZE CAMERAS OK')
+    
+    def initializeAllCameras(self, camera_address_list):
+        self.camera_address_list = camera_address_list
+        print('INITIALIZE CAMERAS OK')
+
 
     def captureImage(self):
         camera_id = 0
         for camera_object in self.camera_object_list:
             if (camera_object.isOpened()):
+                camera_name = 'CAMERA_' + str(camera_id)
                 ret, frame = camera_object.read()
                 if not ret:
-                    print("Cannot open camera!")
+                    print("CANNOT OPEN " + camera_name)
                 else:
-                    cv2.imwrite('Camera_' + str(camera_id) + '.jpg', frame)
-                    print('Camera ' + str(camera_id) + ' captured')
+                    cv2.imwrite(camera_name + '.jpg', frame)
+                    print(camera_name + ' CAPTURED')
             camera_id += 1
 
     def captureVideo(self, videtime_between_image_frame):
@@ -37,4 +47,6 @@ class CameraModule:
     def closeCamera(self):
         for camera_object in self.camera_object_list:
             camera_object.release()
+        print('CAMERAS TURNED OFF')
+
 
--- receive.py
+++ receive.py
@@ -2,35 +2,35 @@ import os
 import can
 from CameraModules import CameraModule
 
-#msg = can.Message(arbitration_id=0x123, data=[0, 1, 2, 3, 4, 5, 6, 7], is_extended_id=False)
-
 def setup_can_object():
     os.system('sudo ip link set can0 type can bitrate 100000')
     os.system('sudo ifconfig can0 up')
     can0 = can.interface.Bus(channel = 'can0', bustype = 'socketcan')# socketcan_native
+    print('CAN SETUP OK')
     return can0
 
 def do_something(RM_speed, cameraModule):
     print("Current RM speed received is: " + str(RM_speed))
     if (RM_speed > 10):
+        print("RM SPEED GREATER THAN THRESHOLD. TAKING PICTURES")
         cameraModule.captureImage()
 
 
 if __name__ == "__main__":
     can0 = setup_can_object()
-    print('CAN SETUP OK')
     cameraModule = CameraModule()
     cameraModule.addCamera('/dev/video0')
-    print('ADD CAMERAS OK')
     cameraModule.initializeAllCameras()
-    print('INITIALIZE CAMERAS OK')
-    while True:
-        msg = can0.recv()
-        if msg is None:
-            print("Timeout occured. No message")
-        else:
-            print(msg)
-            RM_speed = int.from_bytes(msg.data, byteorder='little')
-            do_something(RM_speed=RM_speed, cameraModule=cameraModule)
 
-    os.system('sudo ifconfig can0 down')
+    while True:
+        try:
+            msg = can0.recv(10.0)
+            if msg is None:
+                print("Timeout occured. No message")
+            else:
+                print(msg)
+                RM_speed = int.from_bytes(msg.data, byteorder='little')
+                do_something(RM_speed=RM_speed, cameraModule=cameraModule)
+        except KeyboardInterrupt:
+            cameraModule.closeCamera()
+            os.system('sudo ifconfig can0 down')
