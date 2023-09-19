d045785d45e8aebc77825c56a4797406a584e941
lennoxtr <huykhang.tranp@gmail.com>
2023-09-19 14:09:57 +0200

    allowed changing ssh_pass file and dashboard server address

--- dashboard_handler.py
+++ dashboard_handler.py
@@ -15,8 +15,10 @@ class CanHandler:
     def can_down():
         os.system('sudo ifconfig can0 down')
     
-class DashboardDataHandler:
-    def __init__(self, RM_speed_threshold, camera_address_list):
+class DashboardHandler:
+    def __init__(self, ssh_pass_file, dashboard_server_address, RM_speed_threshold, camera_address_list):
+        self.ssh_pass_file = ssh_pass_file
+        self.dashboard_server_address = dashboard_server_address
         self.canHandler = CanHandler.setup_can()
         self.cameraHandler = CameraHandler(RM_speed_threshold, camera_address_list)
 
@@ -42,8 +44,13 @@ class DashboardDataHandler:
 
 
 if __name__ == "__main__":
+    ssh_pass_file = "ssh_pass"
+    dashboard_server_address = "hakan@7.tcp.eu.ngrok.io"
     camera_address_list = ['/dev/video0', '/dev/video2']
     RM_speed_threshold = 10
 
-    dashboardDataHandler = DashboardDataHandler(RM_speed_threshold, camera_address_list)
-    dashboardDataHandler.handleMessage()
+    dashboardHandler = DashboardHandler(ssh_pass_file= ssh_pass_file,
+                                                dashboard_server_address= dashboard_server_address,
+                                                RM_speed_threshold= RM_speed_threshold,
+                                                camera_address_list= camera_address_list)
+    dashboardHandler.handleMessage()
